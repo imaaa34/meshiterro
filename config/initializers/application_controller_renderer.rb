828f01e78174eb8dbf04abb6c76ce1a5fb2d8f96
imaaa34 <oct.imd34@gmail.com>
2021-04-16 01:42:24 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b53d7b53f7d195bbb4584b374d62a8632c11156ed061f666516e8d5f9bf60d038da8c920149ef27a1002073d513de3982c98ec00bd1f87fc063ed0ab3ae08dfe'
