e1308e8ba77802d2bff4d41f261b9b5f85f89dbf
imaaa34 <oct.imd34@gmail.com>
2021-04-19 14:07:17 +0000

    [Update] topページからログイン・新規登録ページへリンク設置、プロフィール編集画面変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  post 'homes' => 'devise/sessions#create'
+  get 'homes' => 'devise/sessions#new'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
